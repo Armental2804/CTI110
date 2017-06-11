3bf77b6366081ccceb3bfe37f6972d8851b9babb
Armental2804 <armental2804@student.faytechcc.edu>
2017-06-10 23:07:54 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_Armenta.py
@@ -0,0 +1,11 @@
+# Distance Traveled project
+# 20170610
+# CTI-110 M2HW1- Distance Traveled
+# Loraine Armenta
+#
+#
+print ('The distance traveled for 6 hours is', 70*6)
+
+print ('The distance traveled for 10 hours is', 70*10)
+
+print ('The distance traveled for 15 hours is', 70*15)
